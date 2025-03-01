d2ae4d6cd2b8bd8858cde8b44cc8bdb68bfa4794
Maheerah Anwar <maheerahanwar@gmail.com>
2025-02-28 19:14:50 -0500

    fixing format BEM

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,12 +3,16 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photoId, location, imageSource, username, profile }) => {
   return (
-    <div className="photo-list__item" key={photoId}> {/* ✅ Use photoId as key */}
-      <img src={imageSource} alt={`Photo taken by ${username}`} />
+    <div className="photo-list__item" key={photoId}>
+      <img src={imageSource} alt={`Photo taken by ${username}`} className="photo-list__image"/>
       <div className="photo-list__user-details">
         <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
-        <p>{username}</p>
-        <p>{location.city}, {location.country}</p>
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <div className="photo-list__user-location">
+            <p>{location.city}, {location.country}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
